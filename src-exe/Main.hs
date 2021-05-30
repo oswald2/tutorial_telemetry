c9dfa139be4d7f3c9d0cab9053f4c5b807ae772b
Michael Oswald <michael.oswald@onikudaki.net>
2021-05-30 19:45:04 +0200

    Second Video: connecting and reading bytestrings

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -2,11 +2,31 @@ module Main where
 
 import qualified MyLib                          ( someFunc )
 import           RIO
+import qualified RIO.ByteString                as BS
+import qualified RIO.Text                      as T
 
+import           Conduit
+import           Data.Conduit.Network
 import qualified Data.Text.IO                  as T
+
+
+bsLengthC :: (MonadIO m) => ConduitT ByteString Void m ()
+bsLengthC = awaitForever $ \bs -> liftIO $ T.putStrLn (T.pack (show (BS.length bs)))
 
 
 main :: IO ()
 main = do
-    T.putStrLn "Hello, Haskell!"
-    MyLib.someFunc
+
+    let settings = clientSettings 2502 "localhost"
+
+        connectClient = do 
+            res <- try $ runGeneralTCPClient settings $ \appData -> do
+                runConduitRes $ appSource appData .| bsLengthC
+            case res of 
+                Left (e :: SomeException) -> do 
+                    T.putStrLn "Could not connect, reconnecting..."
+                    threadDelay 2_000_000
+                    connectClient
+                Right _ -> connectClient 
+
+    connectClient
