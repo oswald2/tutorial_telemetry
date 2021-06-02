1db293b2e6e5ac8059826f447109474ce4c8b3c4
Michael Oswald <michael.oswald@onikudaki.net>
2021-06-02 16:08:12 +0200

    Video 4: command line parsing

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE 
+    TypeOperators 
+    , DataKinds
+    , StandaloneDeriving
+    , FlexibleInstances
+#-}
 module Main where
 
 import           RIO
@@ -5,10 +11,10 @@ import qualified RIO.ByteString                as BS
 import qualified RIO.Text                      as T
 
 import           Conduit
+import           Config
 import           Data.Conduit.Network
 import qualified Data.Text.IO                  as T
-
-import           Config
+import           Options.Generic
 
 
 bsLengthC :: (MonadIO m) => ConduitT ByteString Void m ()
@@ -16,20 +22,51 @@ bsLengthC =
     awaitForever $ \bs -> liftIO $ T.putStrLn (T.pack (show (BS.length bs)))
 
 
+connectClient :: ClientSettings -> IO ()
+connectClient settings = do
+    res <- try $ runGeneralTCPClient settings $ \appData -> do
+        runConduitRes $ appSource appData .| bsLengthC
+    case res of
+        Left (_ :: SomeException) -> do
+            T.putStrLn "Could not connect, reconnecting..."
+            threadDelay 2_000_000
+            connectClient settings
+        Right _ -> connectClient settings
+
+
+data Options w = Options
+    { version :: w ::: Bool <?> "Display version information"
+    , config  :: w ::: Maybe String <?> "Specify a config file"
+    , writeDefaultConfig :: w ::: Bool <?> "Write default config to a file"
+    }
+    deriving Generic
+
+instance ParseRecord (Options Wrapped)
+deriving instance Show (Options Wrapped)
+
+
+
 main :: IO ()
 main = do
+    opts <- unwrapRecord "tutorial-telemetry"
+    when (version opts) $ do
+        T.putStrLn "tutorial-telemeter version: 1.0.0"
+        exitSuccess
+    when (writeDefaultConfig opts) $ do 
+        writeConfig defaultConfig "DefaultConfig.json"
+        T.putStrLn "Wrote default config into 'DefaultConfig.json'."
+        exitSuccess
+    cfg <- case config opts of 
+        Nothing -> pure defaultConfig 
+        Just file -> do 
+            res <- readConfig file 
+            case res of 
+                Left err -> do 
+                    T.putStrLn err
+                    exitFailure 
+                Right c -> pure c
 
-    let settings      = clientSettings (fromIntegral (cfgPort cfg)) (encodeUtf8 (cfgHostname cfg))
-        cfg           = defaultConfig
+    let settings = clientSettings (fromIntegral (cfgPort cfg))
+                                  (encodeUtf8 (cfgHostname cfg))
 
-        connectClient = do
-            res <- try $ runGeneralTCPClient settings $ \appData -> do
-                runConduitRes $ appSource appData .| bsLengthC
-            case res of
-                Left (e :: SomeException) -> do
-                    T.putStrLn "Could not connect, reconnecting..."
-                    threadDelay 2_000_000
-                    connectClient
-                Right _ -> connectClient
-
-    connectClient
+    connectClient settings
