3fee07424d03a3c5a20cf7b8d5a9232b090f4e38
Michael Oswald <michael.oswald@onikudaki.net>
2021-06-12 20:02:20 +0200

    Video 8 Addendum

--- src/NcduToTMFrame.hs
+++ src/NcduToTMFrame.hs
@@ -12,9 +12,9 @@ import           Data.Attoparsec.ByteString
 
 import           Data.Time.Clock
 
+import           AppState
 import           CDSTime
 import           CRC
-import           Config
 import           NCTRS
 import           TMFrame
 
@@ -27,19 +27,25 @@ data TMFrameMeta = TMFrameMeta
     deriving Show
 
 
-ncduToTMFrameC :: (MonadIO m, MonadReader env m, HasLogFunc env) => Config -> ConduitT NcduTM TMFrameMeta m ()
-ncduToTMFrameC cfg = awaitForever $ \ncdu -> do
-    let dat = ncduData ncdu
-    if checkCRC dat
-        then do
-            case parseOnly (tmFrameParser cfg) dat of
-                Left err -> do
-                    logError $  "Error parsing TM Frame: " <> display (T.pack err)
-                Right frame -> do
-                    let meta = TMFrameMeta { metaERT = toUTCTime (ncduERT ncdu)
-                                           , metaQuality = ncduQuality ncdu
-                                           , metaFrame = frame
-                                           }
-                    yield meta
-        else do
-            logError $ "CRC error on frame: " <> display (T.pack (show dat))
+ncduToTMFrameC
+    :: (MonadIO m, MonadReader env m, HasLogFunc env, HasConfig env)
+    => ConduitT NcduTM TMFrameMeta m ()
+ncduToTMFrameC = do
+    cfg <- getConfig <$> ask
+    awaitForever $ \ncdu -> do
+        let dat = ncduData ncdu
+        if checkCRC dat
+            then do
+                case parseOnly (tmFrameParser cfg) dat of
+                    Left err -> do
+                        logError $ "Error parsing TM Frame: " <> display
+                            (T.pack err)
+                    Right frame -> do
+                        let meta = TMFrameMeta
+                                { metaERT     = toUTCTime (ncduERT ncdu)
+                                , metaQuality = ncduQuality ncdu
+                                , metaFrame   = frame
+                                }
+                        yield meta
+            else do
+                logError $ "CRC error on frame: " <> display (T.pack (show dat))
